02e4b62a7837f6c0ec948527860082a1c900e33c
Mero <wxg1297@gmail.com>
2022-09-23 21:31:23 +0900

    Initial Commit

---
+++ jjbaksa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  jjbaksa
+//
+//  Created by 정태훈 on 2022/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jjbaksa/jjbaksaApp.swift
@@ -0,0 +1,17 @@
+//
+//  jjbaksaApp.swift
+//  jjbaksa
+//
+//  Created by 정태훈 on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct jjbaksaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
