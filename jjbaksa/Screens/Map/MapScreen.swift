0a197f6e1aa56dfbe3b9bc4c9263075b03a96d5c
JeongYeongJoon <71048593+JeongYeongJoon@users.noreply.github.com>
2022-11-29 21:05:59 +0900

    map_02

--- jjbaksa/Screens/Map/MapScreen.swift
+++ jjbaksa/Screens/Map/MapScreen.swift
@@ -9,47 +9,105 @@ import SwiftUI
 import NMapsMap
 
 struct MapScreen: View {
-    @State var coord: (Double, Double) = (126.9784147, 37.5666805)
-    
+    @State var coord: (Double, Double) = (127.96723316031301, 35.70068507556328)
+    @State var zoom: Double = 6.0
+    @State var searchText: String = ""
     var body: some View {
-        ZStack {
-            VStack {
-                Button(action: {coord = (129.05562775, 35.1379222)}) {
-                    Text("Move to Busan")
+        VStack(spacing: 0) {
+            ZStack {
+                UIMapView(coord: coord, zoom: zoom)
+                    .edgesIgnoringSafeArea(.vertical)
+                VStack(spacing: 0) {
+                    ZStack{
+                        TextField("검색어를 입력해주세요.", text: $searchText)
+                            .keyboardType(.asciiCapable)
+                            .autocorrectionDisabled(true)
+                            .autocapitalization(.none)
+                            .font(.system(size: 12))
+                            .foregroundColor(.base)
+                            .padding(.leading, 10)
+                        Button(action: { () }){
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.textMain)
+                                .font(.system(size: 18))
+                        }
+                        .padding(.leading, 300)
+                        
+                        
+                    }
+                    .frame(width: 343, height: 30)
+                    .background(Capsule().fill(Color.line))
+                    .padding(.top, 10)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 0) {
+                        Button(action: { zoom -= 1 }) {
+                            ZStack {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.textSub)
+                                Image(systemName: "minus.circle")
+                                    .foregroundColor(.textMain)
+                            }
+                            .font(.system(size: 26))
+                        }
+                        Button(action: { zoom += 1 }) {
+                            ZStack {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.textSub)
+                                Image(systemName: "plus.circle")
+                                    .foregroundColor(.textMain)
+                            }
+                            .font(.system(size: 26))
+                        }
+                        Button(action: { () }) {
+                            ZStack {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.textSub)
+                                Image(systemName: "line.3.horizontal.circle.fill")
+                                    .foregroundColor(.textMain)
+                            }
+                            .font(.system(size: 26))
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing, 16)
                 }
-                Button(action: {coord = (127.269311, 37.413294)}) {
-                    Text("Move to Seoul somewhere")
-                }
-                Spacer()
             }
             .zIndex(1)
-            
-            UIMapView(coord: coord)
-                .edgesIgnoringSafeArea(.vertical)
         }
-        
     }
 }
 
 
 struct UIMapView: UIViewRepresentable {
     var coord: (Double, Double)
-  
+    var zoom: Double
+    
     func makeUIView(context: Context) -> NMFNaverMapView {
         let view = NMFNaverMapView()
-        view.showZoomControls = true
+        view.showZoomControls = false
+        view.showScaleBar = false
         view.mapView.positionMode = .direction
-        view.mapView.zoomLevel = 17
-      
+        view.mapView.minZoomLevel = 5
+        view.mapView.zoomLevel = zoom
+        view.mapView.mapType = .navi
+        view.mapView.isZoomGestureEnabled = true
+        view.mapView.isRotateGestureEnabled = false
+        view.mapView.setLayerGroup(NMF_LAYER_GROUP_BUILDING, isEnabled: true)
         return view
     }
     
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        let coord = NMGLatLng(lat: coord.1, lng: coord.0)
-        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
-        cameraUpdate.animation = .fly
-        cameraUpdate.animationDuration = 1
-        uiView.mapView.moveCamera(cameraUpdate)}
+        //let coord = NMGLatLng(lat: coord.1, lng: coord.0)
+        //let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+        //cameraUpdate.animation = .fly
+        //cameraUpdate.animationDuration = 1
+        //uiView.mapView.moveCamera(cameraUpdate)
+        uiView.mapView.zoomLevel = zoom
+    }
+    
+    
 }
 
 struct MapScreen_Previews: PreviewProvider {
