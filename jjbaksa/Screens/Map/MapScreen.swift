898f82f2150b62203702b2e7f8e8cfd97b33b465
Mero <wxg1297@gmail.com>
2022-12-27 15:29:37 +0900

    connect zoom

--- jjbaksa/Screens/Map/MapScreen.swift
+++ jjbaksa/Screens/Map/MapScreen.swift
@@ -15,66 +15,70 @@ struct MapScreen: View {
     var body: some View {
         VStack(spacing: 0) {
             ZStack {
-                UIMapView(coord: coord, zoom: zoom)
-                    .edgesIgnoringSafeArea(.vertical)
+                UIMapView(coord, zoom)
+                        .edgesIgnoringSafeArea(.vertical)
                 VStack(spacing: 0) {
-                    ZStack{
+                    ZStack {
                         TextField("검색어를 입력해주세요.", text: $searchText)
-                            .keyboardType(.asciiCapable)
-                            .autocorrectionDisabled(true)
-                            .autocapitalization(.none)
-                            .font(.system(size: 12))
-                            .foregroundColor(.base)
-                            .padding(.leading, 10)
-                        Button(action: { () }){
+                                .keyboardType(.asciiCapable)
+                                .autocorrectionDisabled(true)
+                                .autocapitalization(.none)
+                                .font(.system(size: 12))
+                                .foregroundColor(.base)
+                                .padding(.leading, 10)
+                        Button(action: { () }) {
                             Image(systemName: "magnifyingglass")
-                                .foregroundColor(.textMain)
-                                .font(.system(size: 18))
+                                    .foregroundColor(.textMain)
+                                    .font(.system(size: 18))
                         }
-                        .padding(.leading, 300)
-                        
-                        
+                                .padding(.leading, 300)
+
+
                     }
-                    .frame(width: 343, height: 30)
-                    .background(Capsule().fill(Color.line))
-                    .padding(.top, 10)
-                    
+                            .frame(width: 343, height: 30)
+                            .background(Capsule().fill(Color.line))
+                            .padding(.top, 10)
+
                     Spacer()
-                    
+
                     VStack(spacing: 0) {
-                        Button(action: { zoom -= 1 }) {
+                        Button(action: {
+                            zoom -= 1
+                        }) {
                             ZStack {
                                 Image(systemName: "circle.fill")
-                                    .foregroundColor(.textSub)
+                                        .foregroundColor(.textSub)
                                 Image(systemName: "minus.circle")
-                                    .foregroundColor(.textMain)
+                                        .foregroundColor(.textMain)
                             }
-                            .font(.system(size: 26))
+                                    .font(.system(size: 26))
                         }
-                        Button(action: { zoom += 1 }) {
+                        Button(action: {
+                            zoom += 1
+                        }) {
                             ZStack {
                                 Image(systemName: "circle.fill")
-                                    .foregroundColor(.textSub)
+                                        .foregroundColor(.textSub)
                                 Image(systemName: "plus.circle")
-                                    .foregroundColor(.textMain)
+                                        .foregroundColor(.textMain)
                             }
-                            .font(.system(size: 26))
+                                    .font(.system(size: 26))
                         }
                         Button(action: { () }) {
                             ZStack {
                                 Image(systemName: "circle.fill")
-                                    .foregroundColor(.textSub)
+                                        .foregroundColor(.textSub)
                                 Image(systemName: "line.3.horizontal.circle.fill")
-                                    .foregroundColor(.textMain)
+                                        .foregroundColor(.textMain)
                             }
-                            .font(.system(size: 26))
+                                    .font(.system(size: 26))
                         }
                     }
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .padding(.trailing, 16)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing, 16)
                 }
             }
-            .zIndex(1)
+                    .zIndex(1)
         }
     }
 }
@@ -83,31 +87,65 @@ struct MapScreen: View {
 struct UIMapView: UIViewRepresentable {
     var coord: (Double, Double)
     var zoom: Double
-    
+
+    init(_ coord: (Double, Double), _ zoom: Double) {
+        self.coord = coord
+        self.zoom = zoom
+    }
+
     func makeUIView(context: Context) -> NMFNaverMapView {
         let view = NMFNaverMapView()
         view.showZoomControls = false
         view.showScaleBar = false
         view.mapView.positionMode = .direction
-        view.mapView.minZoomLevel = 5
         view.mapView.zoomLevel = zoom
-        view.mapView.mapType = .navi
+        view.mapView.mapType = .basic
         view.mapView.isZoomGestureEnabled = true
         view.mapView.isRotateGestureEnabled = false
         view.mapView.setLayerGroup(NMF_LAYER_GROUP_BUILDING, isEnabled: true)
+        view.mapView.addCameraDelegate(delegate: context.coordinator)
+        view.mapView.addOptionDelegate(delegate: context.coordinator)
         return view
     }
-    
-    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        //let coord = NMGLatLng(lat: coord.1, lng: coord.0)
-        //let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
-        //cameraUpdate.animation = .fly
-        //cameraUpdate.animationDuration = 1
-        //uiView.mapView.moveCamera(cameraUpdate)
-        uiView.mapView.zoomLevel = zoom
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(coord, zoom)
     }
-    
-    
+
+    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
+        let coord = NMGLatLng(lat: coord.1, lng: coord.0)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord, zoomTo: zoom)
+        cameraUpdate.animation = .fly
+        cameraUpdate.animationDuration = 1
+        uiView.mapView.moveCamera(cameraUpdate)
+    }
+
+
+    class Coordinator: NSObject, NMFMapViewCameraDelegate, NMFMapViewOptionDelegate {
+        var coord: (Double, Double)
+        var zoom: Double
+
+        init(_ coord: (Double, Double), _ zoom: Double) {
+            self.coord = coord
+            self.zoom = zoom
+        }
+
+        func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
+
+        }
+
+        func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
+
+        }
+
+        func mapViewOptionChanged(_ mapView: NMFMapView) {
+
+        }
+
+
+    }
+
+
 }
 
 struct MapScreen_Previews: PreviewProvider {
