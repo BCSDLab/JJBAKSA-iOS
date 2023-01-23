27f7aa925ceaf7d90ffc2086716abc53b581d6d6
JeongYeongJoon <71048593+JeongYeongJoon@users.noreply.github.com>
2023-01-23 16:05:33 +0900

    add textEditor and ImagePicker

--- jjbaksa/Screens/Map/PostScreen.swift
+++ jjbaksa/Screens/Map/PostScreen.swift
@@ -6,13 +6,16 @@
 //
 
 import SwiftUI
+import UIKit
 import RichEditorView
 
 struct PostScreen: View {
     @State var post: String = ""
     @State var editorController: RichEditorView = RichEditorView(frame: .zero)
-    @State private var isEditingRichText = false
-
+    @State var isShowTextToolBar: Bool = false
+    @State var isShowPhotoLibrary: Bool = false
+    @State var imgArr: [UIImage] = []
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
@@ -22,13 +25,20 @@ struct PostScreen: View {
 
                 ZStack {
                     VStack(spacing: 0) {
-                        if isEditingRichText {
-                            Text("Is editing...")
-                        } else {
-                            Text("Not editing...")
+                        if !(imgArr.isEmpty) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 0) {
+                                    ForEach(0..<imgArr.count, id:\.self) { idx in
+                                        Image(uiImage:imgArr[idx])
+                                            .resizable()
+                                            .frame(width: 80, height: 80)
+                                            .padding(8)
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            }
                         }
-
-                        RichTextEditor(controller: $editorController, isEditingRichText: $isEditingRichText)
+                        RichTextEditor(controller: $editorController)
                                 .font(.system(size: 12))
                                 .padding(.top, 16)
                                 .padding(.leading, 24)
@@ -39,7 +49,7 @@ struct PostScreen: View {
                     VStack(spacing: 0) {
                         Spacer()
                         HStack(spacing: 0) {
-                            Button(action: { () }) {
+                            Button(action: { isShowPhotoLibrary = true }) {
                                 ZStack {
                                     Circle()
                                             .frame(width: 40, height: 40)
@@ -49,22 +59,70 @@ struct PostScreen: View {
                                             .foregroundColor(.textMain)
                                 }
                             }
-                                    .padding(.leading, 16)
-
-                            Button(action: {
-                                editorController.bold()
-                            }) { //TODO: RichTextEditor Tools
+                            .padding(.leading, 16)
+                            ZStack(alignment: .leading) {
                                 ZStack {
-                                    Circle()
+                                    Capsule()
+                                        .frame(width: isShowTextToolBar ? 137 : 40, height: 40)
+                                        .foregroundColor(.base)
+                                        .animation(.easeIn.speed(isShowTextToolBar ? 2.0 : 1.0), value: isShowTextToolBar)
+                                    if isShowTextToolBar {
+                                        HStack(spacing: 0) {
+                                            Button(action: {
+                                                editorController.setFontSize(16)
+                                            }) {
+                                                Text("H")
+                                                    .font(.system(size: 16))
+                                                    .foregroundColor(.textMain)
+                                            }
+                                            .padding(.trailing, 16)
+                                            
+                                            Button(action: {
+                                                editorController.setFontSize(12)
+                                            }) {
+                                                Text("H")
+                                                    .font(.system(size: 12))
+                                                    .foregroundColor(.textMain)
+                                            }
+                                            .padding(.trailing, 16)
+                                            
+                                            Button(action: {
+                                                editorController.strikethrough()
+                                            }) {
+                                                ZStack {
+                                                    Image(systemName: "minus")
+                                                        .font(.system(size: 15))
+                                                    Text("T")
+                                                        .font(.system(size: 18))
+                                                        
+                                                }
+                                                
+                                                .foregroundColor(.textMain)
+                                            }
+                                            .padding(.trailing, 16)
+                                        }
+                                        .padding(.leading, 56)
+                                        .transition(.offset(x: isShowTextToolBar ? -24 : 0))
+                                        .animation(.easeIn.speed(isShowTextToolBar ? 1.5 : 3.0))
+                                    }
+                                }
+                                Button(action: {
+                                    print(imgArr.count)
+                                    isShowTextToolBar.toggle()
+                                    //editorController.strikethrough()
+                                }) { //TODO: RichTextEditor Tools
+                                    ZStack {
+                                        Circle()
                                             .frame(width: 40, height: 40)
                                             .foregroundColor(.line)
-                                    Text("T")
+                                        Text("T")
                                             .font(.system(size: 24))
                                             .foregroundColor(.textMain)
+                                    }
                                 }
+                                
                             }
-                                    .padding(.leading, 8)
-
+                            .padding(.leading, 8)
                             Spacer()
 
                             //TODO: 저장 버튼 Color 수정
@@ -84,6 +142,9 @@ struct PostScreen: View {
                 }
                 Spacer()
             }
+            .sheet(isPresented: $isShowPhotoLibrary) {
+                        ImagePicker(sourceType: .photoLibrary, imageArray: $imgArr)
+                    }
         }
                 .navigationBarBackButtonHidden(false)
     }
@@ -91,7 +152,6 @@ struct PostScreen: View {
 
 struct RichTextEditor: UIViewRepresentable {
     @Binding var controller: RichEditorView
-    @Binding var isEditingRichText: Bool
 
     class Coordinator: RichEditorDelegate {
         var parent: RichTextEditor
@@ -100,13 +160,6 @@ struct RichTextEditor: UIViewRepresentable {
             self.parent = parent
         }
 
-        func richEditorTookFocus(_ editor: RichEditorView) {
-            parent.isEditingRichText = true
-        }
-
-        func richEditorLostFocus(_ editor: RichEditorView) {
-            parent.isEditingRichText = false
-        }
     }
 
     func makeCoordinator() -> Coordinator {
@@ -117,11 +170,11 @@ struct RichTextEditor: UIViewRepresentable {
         controller.isScrollEnabled = false
         controller.delegate = context.coordinator
 
-        let toolbar = RichEditorToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
-        toolbar.options = RichEditorDefaultOption.all
-        toolbar.editor = controller
+        //let toolbar = RichEditorToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
+        //toolbar.options = RichEditorDefaultOption.all
+        //toolbar.editor = controller
 
-        controller.inputAccessoryView = toolbar
+        //controller.inputAccessoryView = toolbar
 
         return controller
     }
@@ -130,6 +183,51 @@ struct RichTextEditor: UIViewRepresentable {
         uiView.html = controller.html
     }
 
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    @Binding var imageArray: [UIImage]
+    @Environment(\.presentationMode) private var presentationMode
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                
+                parent.imageArray.append(image)
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
 }
 
 struct PostScreen_Previews: PreviewProvider {
